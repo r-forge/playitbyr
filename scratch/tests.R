2aea11a6b8d59d4d1348e4aa9992049b7c473ae0
statisfactions (Ethan Brown) <ethancbrown+rforge@gmail.com>
2011-06-20 11:12:00 +0000

    At thinktank, partially working, version of R has no system2 function! Neet to change.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/playitbyr@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- scratch/tests.R
+++ scratch/tests.R
@@ -7,6 +7,7 @@ x <- sonify(data=iris,
       pitch = "Petal.Length", 
       timbre=1, dur=5, vol=0.75), 
       rendering="audio")
+
 x <- (x + shape_notes() 
    + scale_time_linear(0, 3) 
    + scale_pitch_linear(3, 13))
