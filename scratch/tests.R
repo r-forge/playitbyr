84a92f9162d7cabfd8b438440745b72285da7de0
statisfactions (Ethan Brown) <ethancbrown+rforge@gmail.com>
2011-07-10 21:41:52 +0000

    A version that actually builds and works correctly! Hurrah
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/playitbyr@39 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- scratch/tests.R
+++ scratch/tests.R
@@ -1,17 +1,15 @@
 source("aaa.R")
 source("sonify.R")
-source("scaling.R")
+source("sonscaling.R")
 source("Rrender.R")
 require(audio)
 
 x <- sonify(data=iris, 
       sonaes(time="Petal.Width",
       pitch = "Petal.Length", 
-      timbre=1, dur=5, vol=0.75), 
-      rendering="audio")
+      timbre=1, dur=5, vol=0.75))
 
-x <- (x + shape_notes() 
-   + scale_time_linear(0, 3) 
+x <- (x + scale_time_linear(0, 3))
    + scale_pitch_linear(3, 13))
 
 x
