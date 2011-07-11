c40b6962a5ec07d7ab1a5380235853d1501eadac
statisfactions (Ethan Brown) <ethancbrown+rforge@gmail.com>
2011-07-11 19:26:43 +0000

    Slight cleanup of sources; also changed playAudioRendering to no longer mess with audio instances which works better on Windows.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/playitbyr@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- scratch/tests.R
+++ scratch/tests.R
@@ -1,15 +1,13 @@
-source("aaa.R")
-source("sonify.R")
-source("sonscaling.R")
-source("Rrender.R")
-require(audio)
+setPlayer("aplay")
 
 x <- sonify(data=iris, 
       sonaes(time="Petal.Width",
       pitch = "Petal.Length", 
       timbre=1, dur=5, vol=0.75))
 
-x <- (x + scale_time_linear(0, 3))
+x <- (x + scale_time_linear(0, 3)
    + scale_pitch_linear(3, 13))
 
 x
+
+playLastRendering()
