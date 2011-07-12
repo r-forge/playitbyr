bc9b9b581ccfa6d56e6e3f7fb41df3fa9a23f4cf
statisfactions (Ethan Brown) <ethancbrown+rforge@gmail.com>
2011-07-12 00:10:51 +0000

    Made changes to getPlayer() and setPlayer() process to simplify.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/playitbyr@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- scratch/tests.R
+++ scratch/tests.R
@@ -1,13 +1,13 @@
 setPlayer("aplay")
 
-x <- sonify(data=iris, 
+x <- (sonify(data=iris, 
       sonaes(time="Petal.Width",
       pitch = "Petal.Length", 
       timbre=1, dur=5, vol=0.75))
-
-x <- (x + scale_time_linear(0, 3)
-   + scale_pitch_linear(3, 13))
-
+      + scale_time_linear(0, 3)
+      + scale_pitch_linear(3, 13))
 x
 
 playLastRendering()
+
+setPlayer("aplay")
