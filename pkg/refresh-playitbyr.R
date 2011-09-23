cb40ba1ae76f7c4bfb6bab3f9f30a7ac17a5d001
statisfactions (Ethan Brown) <ethancbrown+rforge@gmail.com>
2011-09-23 02:36:29 +0000

    added a utility script for dev purposes, refresh-playitbyr.R ... just source this to unload and reload and updated version of the package (see its comments)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/playitbyr@100 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/refresh-playitbyr.R
@@ -0,0 +1,27 @@
+## This script refreshes your current installation of playitbyr after
+## making changes to the source and reloads it into R. It assumes that your
+## working directory is the parent directory of playitbyr
+##
+## WARNING: ?detach says 'detaching and re-attaching a package may not
+## refresh some or all components of the package, and is inadvisable.'
+## In practice I've found that this works so far with playitbyr.
+## 
+## If wanted for quick testing of .R files and not wanting to do a
+## formal package load, try either:
+##
+## lapply(dir("playitbyr/R/", full.name=T), source)
+##
+## or, from the R.utils package,
+##
+## require(R.utils)
+## sourceDirectory("playitbyr/R/", modifiedOnly=TRUE)
+##
+## (courtesy Hadley Wickam and Henrik Bengtsson, respectively)
+
+if("package:playitbyr" %in% search())
+  detach("package:playitbyr", unload=TRUE)
+remove.packages("playitbyr")
+system("R CMD build playitbyr") # may not work on Windows
+install.packages("playitbyr_0.1-2.tar.gz")
+require(playitbyr)
+
